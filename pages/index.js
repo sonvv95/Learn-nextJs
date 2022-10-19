f6f61c89614d45f60dc06f8a85994526b9cbc970
Vu Van Son <vuson8910@gmail.com>
2022-10-19 22:47:31 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
       <div>sơn test</div>
       <div>bongs ddas</div>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -68,7 +68,7 @@ export default function Home() {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
